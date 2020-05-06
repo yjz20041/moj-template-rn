0cd4a04669010c627613fb11b295cc2c9709e3b0
杨介正 <hzyangjiezheng@corp.netease.com>
2020-05-06 13:58:12 +0800

    feat: temp

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,6 +1,48 @@
 import Fetch, { setProxyConfig } from '@music/mosi-rn-util/es/fetch';
+import RPC from '@music/mosi-rn-util/es/rpc';
 import { proxyConfig } from '@config/app.config';
 
 setProxyConfig(proxyConfig);
 
-export default Fetch;
+let loadingCount = 0;
+const loadingTimeout = 5000;
+let loadingTimer;
+
+const wrapFetch = (url, options = {}) => {
+    // loading和error的通用逻辑
+    const {
+        loading,
+        error
+    } = options;
+    if (loading === true) {
+        if (loadingCount <= 0) {
+            RPC.showLoading();
+            clearTimeout(loadingTimer);
+            loadingTimer = setTimeout(() => {
+                RPC.hideLoading();
+            }, loadingTimeout);
+        }
+        loadingCount++;
+    }
+    return Fetch(url, options).catch(() => {
+        if (error === true) {
+            RPC.showToast({
+                message: '网络繁忙，请重试',
+                duratin: 1
+            });
+        }
+    }).finally(() => {
+        loadingCount--;
+        if (loadingCount <= 0) {
+            clearTimeout(loadingTimer);
+            RPC.hideLoading();
+        }
+    });
+};
+wrapFetch.post = (url, options) => {
+    // eslint-disable-next-line no-param-reassign
+    options.method = 'post';
+    return wrapFetch(url, options);
+};
+
+export default wrapFetch;
