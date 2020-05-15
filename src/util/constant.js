5aaaaaaedee83c7cdf2dffc34d368420077afc06
杨介正 <hzyangjiezheng@corp.netease.com>
2020-05-15 18:13:44 +0800

    feat: temp

--- src/util/constant.js
+++ src/util/constant.js
@@ -1,8 +1,11 @@
+
+const CACHE_PREFIX = 'HELLO_CACHE';
+
 export const CACHE_KEY = {
   // 用户信息是唯一允许全局的
   USER_INFO: 'GLOBAL_CACHE_USER_INFO',
   // 请用工程名作区分，防止工程间命名冲突
-  TEST: 'PROJECT_CACHE_NAME_TEST'
+  TEST: `${CACHE_PREFIX}_TEST`
 };
 
 export default {};
